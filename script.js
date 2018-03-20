d6b8f43d5a49355d859e038629368be59339367e
SarT0x <quantin.sole@orange.fr>
2018-03-20 13:30:57 +0100

    version 3

--- script.js
+++ script.js
@@ -1,17 +1,19 @@
 
+
+
 Highcharts.chart('container', {
 
     title: {
-        text: 'Solar Employment Growth by Sector, 2010-2016'
+        text: 'Courbe des cryptomonnaies'
     },
 
     subtitle: {
-        text: 'Source: thesolarfoundation.com'
+        text: 'Source: CrytoCompare.com'
     },
 
     yAxis: {
         title: {
-            text: 'Number of Employees'
+            text: 'Prix en €'
         }
     },
     legend: {
@@ -30,29 +32,11 @@ Highcharts.chart('container', {
     },
 
     series: [{
-        name: 'Installation',
-        data: [43934, 52503, 57177, 69658, 97031, 119931, 137133, 154175]
+        name: 'Bitcoin',
+        data: [8537, 9300, 9278, 7809, 8216, 7563, 8632, 8543]
     }, {
-        name: 'Manufacturing',
-        data: [24916, 24064, 29742, 29851, 32490, 30282, 38121, 40434]
-    }, {
-        name: 'Sales & Distribution',
-        data: [11744, 17722, 16005, 19771, 20185, 24377, 32147, 39387]
-    }, {
-        name: 'Project Development',
-        data: [null, null, 7988, 12169, 15112, 22452, 34400, 34227]
-    }, {
-        name: 'Other',
-        data: [12908, 5948, 8105, 11248, 8989, 11816, 18274, 18111]
-    }, {
-        name: 'Other',
-        data: [12908, 11248, 8105, 11248, 8989, 11816, 18274, 18111]
-    }, {
-        name: 'Etherium',
-        data: [12908, 18274, 8105, 11248, 8989, 11248, 18274, 18111]
-    }, {
-        name: 'BitCoin',
-        data: [12908, 5948, 8105, 11248, 8989, 18274, 18274, 18111]
+        name: 'Ethereum',
+        data: [623, 590, 610, 600, 500, 392, 396, 431]
     }],
 
     responsive: {
