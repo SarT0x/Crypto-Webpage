369ee2476dccaf795c835f1285c45afda3ff9b52
SarT0x <pierrechaumais@yahoo.fr>
2018-04-06 17:20:53 +0200

    Fix 1

--- crypto/public/asset/index-js.js
+++ crypto/public/asset/index-js.js
@@ -1,5 +1,6 @@
 $(document).ready(function() {
-
+	var table = [];
+	var tabValue = []
 	$('#buyCrypto').on('click', function(e) {
 		e.preventDefault();
 
@@ -34,23 +35,25 @@ $(document).ready(function() {
 		dataType:'JSON',
 		success:function(data) {
 			console.log(data);
-			var table = [];
-			for (var elt in data.data.Historique){
-				var point = [];
-				var date = (data.data.Historique[elt].time) * 1000;
-				var prix = data.data.Historique[elt].high;
-				point.push(date);
-				point.push(prix);
-				table.push(point);
+			for (var val in data.data) {
+				var name = data.data[val].Nom;
+				for (var elt in data.data[val].Historique){
+					var point = [];
+					var date = (data.data[val].Historique[elt].time) * 1000;
+					var prix = data.data[val].Historique[elt].high;
+
+					point.push(date);
+					point.push(prix);
+					table.push(point);
+				}
+				var value = {
+					'name' : name,
+					'data' : table,
+					'tooltip': {valueDecimals: 2}
+				}
+				tabValue.push(value);
 			}
-			for (var elt in data.data.Historique){
-				var point = [];
-				var date = (data.data.Historique[elt].time) * 1000;
-				var prix = data.data.Historique[elt].high;
-				point.push(date);
-				point.push(prix);
-				table.push(point);
-			}
+			console.log(tabValue);
 			if($('#graph').length > 0) {
 
 
@@ -82,37 +85,7 @@ $(document).ready(function() {
 							pointStart: 2010
 						}
 					},
-					series: [{
-						name: 'BTC',
-						data: [43934, 52503, 57177, 69658, 97031, 119931, 137133, 154175],
-						tooltip: {
-							valueDecimals: 2
-						}
-					}, {
-						name: 'ETC',
-						data: [24916, 24064, 29742, 29851, 32490, 30282, 38121, 40434],
-						tooltip: {
-							valueDecimals: 2
-						}
-					}, {
-						name: 'NEO',
-						data: [11744, 17722, 16005, 19771, 20185, 24377, 32147, 39387],
-						tooltip: {
-							valueDecimals: 2
-						}
-					}, {
-						name: 'EOS',
-						data: [null, null, 7988, 12169, 15112, 22452, 34400, 34227],
-						tooltip: {
-							valueDecimals: 2
-						}
-					}, {
-						name: 'LTC',
-						data: [12908, 5948, 8105, 11248, 8989, 11816, 18274, 18111],
-						tooltip: {
-							valueDecimals: 2
-						}
-					}],
+					series: [table],
 
 					responsive: {
 						rules: [{
