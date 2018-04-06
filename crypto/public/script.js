97fa1f763c9c07275b4d6de33c578f0db7fddc27
SarT0x <pierrechaumais@yahoo.fr>
2018-04-06 16:55:07 +0200

    Front + Back

--- crypto/public/script.js
+++ crypto/public/script.js
@@ -2,29 +2,39 @@
 $(document).ready(function() {
   $('#btdGraph').on('click', function(event) {
     $.ajax({
-      url:'http://localhost:3000/api/currency/5ac78321a31d541d14b89e7f',
+      url:'http://localhost:3000/api/currency/5ac78321a31d541d14b89e80',
       method:'GET',
       dataType:'JSON',
       success:function(data) {
+
+        var table = [];
+        console.log(data.data.Historique);
         var array = [];
         console.log(data.data.Historique[1].high);
         for (var i = 0; i < data.data.Historique.length; i++) {
           array[i] = data.data.Historique[i].high;
 
         }
+
+        for (var elt in data.data.Historique){
+        var point = [];
+        var date = (data.data.Historique[elt].time) * 1000;
+        var prix = data.data.Historique[elt].high;
+        point.push(date);
+        point.push(prix);
+        table.push(point);
+        }
         console.log(array);
         Highcharts.stockChart('container', {
           rangeSelector: {
             selected: 1
           },
-
           title: {
             text: 'Bitcoin'
           },
-
           series: [{
             name: 'Prix',
-            data: array,
+            data: table,
             tooltip: {
               valueDecimals: 1,
               valueSuffix: '€'
